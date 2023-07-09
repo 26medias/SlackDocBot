f703e08d0710822ab257f93a6e29adc99112fb54
Julien Loutre <julien@leap-forward.ca>
2023-07-08 21:43:37 -0400

    Cleanup

--- SlackBot.py
+++ SlackBot.py
@@ -8,90 +8,88 @@ from env import BOT_TOKEN, SIGNING_SECRET, DEEPLAKE_USERNAME, DEEPLAKE_DB
 from RepoAgent import RepoAgent
 import json
 
-# Init the agent
-agent = RepoAgent(deeplake_username=DEEPLAKE_USERNAME, deeplake_db=DEEPLAKE_DB)
 
-# Instantiate Flask 
-app = Flask(__name__)
+class SlackBot:
+    def __init__(self, username, db, token, secret):
+        self.agent = RepoAgent(deeplake_username=username, deeplake_db=db)
+        self.slack_web_client = WebClient(token=token)
+        response = self.slack_web_client.auth_test()
+        self.bot_id = response['user_id']
+        print("Bot ID: ", self.bot_id)
+        self.signature_verifier = SignatureVerifier(secret)
 
-# Initialize a Web API client
-slack_web_client = WebClient(token=BOT_TOKEN)
-response = slack_web_client.auth_test()
-bot_id = response['user_id']
+    def post_agent_response(self, channel, text):
+        try:
+            # Post the markdown answer to the channel where the event was triggered
+            print(f"#{channel}: {text}")
+            response = self.slack_web_client.chat_postMessage(
+                channel=channel,
+                text=self.agent.ask(text)
+            )
+        except SlackApiError as e:
+            print(f"Error: {e}")
+        return True
 
-print("Bot ID: ", bot_id)
+    def handle_ask(self, payload):
+        if isinstance(payload, str):
+            payload = json.loads(payload)
+        user = payload['user_id']
+        text = payload['text']
+        channel = payload['channel_id']
+        try:
+            # Post the markdown answer to the channel where the event was triggered
+            # response = self.slack_web_client.chat_postMessage(
+            #     channel=channel,
+            #     text="I'm looking..."
+            # )
+            # Call the agent in a new thread to avoid timeout
+            thread = threading.Thread(target=self.post_agent_response, args=(channel, text))
+            thread.start()
+        except SlackApiError as e:
+            print(f"Error: {e}")
+        return Response({'status':'ok'}), 200
 
-# Initialize signature verifier
-signature_verifier = SignatureVerifier(SIGNING_SECRET)
-
-def post_agent_response(channel, text):
-    try:
-        # Post the markdown answer to the channel where the event was triggered
-        response = slack_web_client.chat_postMessage(
-            channel=channel,
-            text=agent.ask(text)
-        )
-        print("chat_postMessage", response)
-    except SlackApiError as e:
-        print(f"Error: {e}")
-
-
-@app.route('/ask', methods=['POST'])
-def handle_ask():
-    print("@ask Headers: ", request.headers)
-    if request.headers['Content-Type'] == 'application/x-www-form-urlencoded':
-        payload = request.form
-    else:
-        payload = request.get_data().decode("utf-8")
-    print("Payload: ", json.dumps(payload, indent=4))
-    user = payload['user_id']
-    text = payload['text']
-    channel = payload['channel_id']
-    try:
-        # Post the markdown answer to the channel where the event was triggered
-        response = slack_web_client.chat_postMessage(
-            channel=channel,
-            text="I'm looking..."
-        )
-        print("chat_postMessage", response)
-        # Call the agent in a new thread to avoid timeout
-        thread = threading.Thread(target=post_agent_response, args=(channel, text))
-        thread.start()
-    except SlackApiError as e:
-        print(f"Error: {e}")
-    return Response(), 200
-
-
-@app.route('/mention', methods=['POST'])
-def handle_mention():
-    print("@mention Headers: ", request.headers)
-    if request.headers['Content-Type'] == 'application/x-www-form-urlencoded':
-        payload = request.form
-    else:
-        payload = request.get_data().decode("utf-8")
-    
-    if signature_verifier.is_valid_request(payload, request.headers):
-        # Extract the data from the payload
-        data = request.json
-        print("data", json.dumps(data, indent=4))
-        # Here's the added part for verification
-        if 'challenge' in data:
-            return Response(data['challenge']), 200
-
-        # Extract the text from the payload
-        event = data.get('event', {})
-        print("event", json.dumps(event, indent=4))
-
-        message = event.get('text', "").replace("<@"+bot_id+">", "DocBot")
-        channel = event.get('channel', {})
-        print("message", json.dumps(message, indent=4))
-       
-        thread = threading.Thread(target=post_agent_response, args=(channel, message))
-        thread.start()
-        return Response(), 200
-    else:
-        return Response(), 403
+    def handle_mention(self, payload, headers):
+        if self.signature_verifier.is_valid_request(payload, headers):
+            if isinstance(payload, str):
+                payload = json.loads(payload)
+            # Here's the added part for verification
+            if 'challenge' in payload:
+                return Response(payload['challenge']), 200
+            # Extract the text from the payload
+            event = payload.get('event', {})
+            #print("event", json.dumps(event, indent=4))
+            message = event.get('text', "").replace("<@"+self.bot_id+">", "DocBot")
+            channel = event.get('channel', {})
+            #print("message", json.dumps(message, indent=4))
+            thread = threading.Thread(target=self.post_agent_response, args=(channel, message))
+            thread.start()
+            return Response({'status':'ok'}), 200
+        else:
+            return Response({'status':'invalid_signature'}), 403
 
 
 if __name__ == "__main__":
+    app = Flask(__name__)
+    bot = SlackBot(DEEPLAKE_USERNAME, DEEPLAKE_DB, BOT_TOKEN, SIGNING_SECRET)
+
+    @app.route('/ask', methods=['POST'])
+    def ask():
+        print("@ask Headers: ", request.headers)
+        if request.headers['Content-Type'] == 'application/x-www-form-urlencoded':
+            payload = request.form
+        else:
+            payload = request.get_data().decode("utf-8")
+        print("Payload: ", json.dumps(payload, indent=4))
+        return bot.handle_ask(payload)
+
+    @app.route('/mention', methods=['POST'])
+    def mention():
+        print("@mention Headers: ", request.headers)
+        if request.headers['Content-Type'] == 'application/x-www-form-urlencoded':
+            payload = request.form
+        else:
+            payload = request.get_data().decode("utf-8")
+        return bot.handle_mention(payload, request.headers)
+
     app.run(port=3000)
